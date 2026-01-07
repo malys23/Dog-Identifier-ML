fef9f4817b0208368d485fcc9117dd52610847c9
Alyona M <alyona.maliassova@gmail.com>
2026-01-07 16:03:13 -0600

    Reshaped/flattened data and standardized

--- dogModel.py
+++ dogModel.py
@@ -5,7 +5,6 @@ import h5py
 import scipy
 from PIL import Image
 from scipy import ndimage
-
 import pandas as pd
 
 train_set_x_orig = np.load('train_dogvnondog/train_set_x.npy')
@@ -14,20 +13,17 @@ test_set_x_orig = np.load('test_dogvnondog/test_set_x.npy')
 test_set_y = np.load('test_dogvnondog/test_set_y.npy')
 classes = np.load('train_dogvnondog/list_classes.npy')
 
-#Exercise 1
-
+#1: find values for m_train, m_test, and num_px
 m_train = train_set_x_orig.shape[0]
 m_test = test_set_x_orig.shape[0]
 num_px = train_set_x_orig.shape[1]
 train_set_y = train_set_y.reshape(1, m_train)
 test_set_y = test_set_y.reshape(1, m_test)
 
-print ("Number of training examples: m_train = " + str(m_train))
-print ("Number of testing examples: m_test = " + str(m_test))
-print ("Height/Width of each image: num_px = " + str(num_px))
-print ("Each image is of size: (" + str(num_px) + ", " + str(num_px) + ", 3)")
-print ("train_set_x shape: " + str(train_set_x_orig.shape))
-print ("train_set_y shape: " + str(train_set_y.shape))
-print ("test_set_x shape: " + str(test_set_x_orig.shape))
-print ("test_set_y shape: " + str(test_set_y.shape))
-print ("classes shape: " + str(classes.shape))
+#2: Reshape data sets so images are flattened
+train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0], -1).T
+test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T
+
+#3: Standardize dataset (divide by 255, max val of pixel channel)
+train_set_x = train_set_x_flatten / 255
+test_set_x = test_set_x_flatten / 255
