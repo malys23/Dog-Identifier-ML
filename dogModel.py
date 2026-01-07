5bdd25d8da5678dc7ad590806bfdb089457156ba
Alyona M <alyona.maliassova@gmail.com>
2026-01-07 15:53:27 -0600

    Exercise 1 - reshaping and calling
    Testing if I can find values for the m_train, test, numpx, and reshaping the Y data

--- dogModel.py
+++ dogModel.py
@@ -14,9 +14,20 @@ test_set_x_orig = np.load('test_dogvnondog/test_set_x.npy')
 test_set_y = np.load('test_dogvnondog/test_set_y.npy')
 classes = np.load('train_dogvnondog/list_classes.npy')
 
-#test to see if data loaded
-index = 100
-plt.imshow(train_set_x_orig[index])
-print(train_set_y[index])
-print ("y = " + str(train_set_y[index]) + ", it's a '" + classes[(train_set_y[index])] +  "' picture.")
-plt.show()
+#Exercise 1
+
+m_train = train_set_x_orig.shape[0]
+m_test = test_set_x_orig.shape[0]
+num_px = train_set_x_orig.shape[1]
+train_set_y = train_set_y.reshape(1, m_train)
+test_set_y = test_set_y.reshape(1, m_test)
+
+print ("Number of training examples: m_train = " + str(m_train))
+print ("Number of testing examples: m_test = " + str(m_test))
+print ("Height/Width of each image: num_px = " + str(num_px))
+print ("Each image is of size: (" + str(num_px) + ", " + str(num_px) + ", 3)")
+print ("train_set_x shape: " + str(train_set_x_orig.shape))
+print ("train_set_y shape: " + str(train_set_y.shape))
+print ("test_set_x shape: " + str(test_set_x_orig.shape))
+print ("test_set_y shape: " + str(test_set_y.shape))
+print ("classes shape: " + str(classes.shape))
