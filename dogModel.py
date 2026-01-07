2490af38b748be9073d7160f0d2bcab103a2fef4
Alyona M <alyona.maliassova@gmail.com>
2026-01-07 11:19:54 -0600

    Searching for datasets
    Adding kaggle for dog vs not dog dataset from https://www.kaggle.com/datasets/danielshanbalico/dog-vs-not-dog?resource=download

--- dogModel.py
+++ dogModel.py
@@ -5,6 +5,15 @@ import h5py
 import scipy
 from PIL import Image
 from scipy import ndimage
+
+import kagglehub
+from kagglehub import KaggleDatasetAdapter
 #need to get test and training data and import
 
+#load the data (dog/non-dog)
+dataset = kagglehub.dataset_download("danielshanbalico/dog-vs-not-dog")
+print("Path to dataset files:", dataset)
+
+
+#train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
 
