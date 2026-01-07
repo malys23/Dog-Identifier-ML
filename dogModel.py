b115665aad2f91cebb0f3a882f88366d99560620
Alyona M <alyona.maliassova@gmail.com>
2026-01-07 16:16:31 -0600

    Functions made
    Implemented sigmoid function, initialize_with_zeros function to start the model

--- dogModel.py
+++ dogModel.py
@@ -7,6 +7,8 @@ from PIL import Image
 from scipy import ndimage
 import pandas as pd
 
+from sigmoid import sigmoid
+
 train_set_x_orig = np.load('train_dogvnondog/train_set_x.npy')
 train_set_y = np.load('train_dogvnondog/train_set_y.npy')
 test_set_x_orig = np.load('test_dogvnondog/test_set_x.npy')
@@ -27,3 +29,8 @@ test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T
 #3: Standardize dataset (divide by 255, max val of pixel channel)
 train_set_x = train_set_x_flatten / 255
 test_set_x = test_set_x_flatten / 255
+
+#Testing sigmoid
+x = np.array([0, 2])
+output = sigmoid(x)
+print(output)
